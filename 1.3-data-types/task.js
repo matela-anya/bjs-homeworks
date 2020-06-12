c871b699541da3459df35f78accb7e491e994107
Anna Matela <a-matela@mail.ru>
2020-06-12 20:39:00 +0300

    first commit wrappers-for-primitives

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,30 @@
+"use strict"
 function calculateTotalMortgage(percent, contribution, amount, date) {
     // код для задачи №1 писать здесь
-    // return totalAmount;
+    percent = parseInt(percent, 10) / 100;
+    contribution = parseInt(contribution, 10);
+    amount = parseInt(amount, 10);
+
+    let credit = amount - contribution;
+    let mounth = new Date().getMonth() - date;
+    let payAll = credit * ((percent / 12) + (percent / 12) / (Math.pow((1 + (percent / 12)),  mounth) - 1));
+    let totalAmount = (payAll * mounth).toFixed(2);
+
+    console.log(totalAmount);
+
+    return totalAmount;
 }
 
 function getGreeting(name) {
     // код для задачи №2 писать здесь
-    // return greeting;
+    let greeting;
+
+    if (name) {
+        greeting = `Привет, мир! Меня зовут ${name}`;
+    } else {
+        greeting = `Привет, мир! Меня зовут Аноним`;
+    }
+    
+    console.log(greeting);
+    return greeting;
 }

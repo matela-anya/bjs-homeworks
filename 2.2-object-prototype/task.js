52ea0e674514b5a6f393624addb595f9ba0c2976
Anna Matela <a-matela@mail.ru>
2020-06-16 17:41:47 +0300

    first commit object-prototype

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,9 +1,48 @@
-//String.prototype.isPalindrome - для задачи №1
+// Не понимаю, куда тут пихать this и как связать console.log в задаче с фунцкцией? Как обычно через return? Какой тут должен быть синтаксис? Искала, ничего подобного не нашла. В лекции ответов нет.
+function String.prototype.isPalindrome(str) {
+    str = str.toLowerCase().replace(/\s/g, '');
+  
+    if(str === str.split('').reverse().join('')) {
+      true;
+    } else {
+      false;
+    };
+  };
+  
+  console.log("А роза упала на лапу Азора");
+
+
+function getAnimalSound(animal) {
+    // код для задачи №1 писать здесь. Почему в домашке два задания №1?
+  let animalSound;
+  let sound = animal.sound;
+
+  if (animal === undefined) {
+    animalSound = null;
+  } else {
+    animalSound = sound;
+  };
+
+  return animalSound;
+};
 
 function getAverageMark(marks) {
     // код для задачи №2 писать здесь
-    // return averageMark
-}
+  let average = 0;
+  let roundedAverage;
+
+  if (marks.length === 0) {
+    averageMark = 0;
+  };
+
+  for (let i = 0; i < marks.length; i++) {
+    average += marks[i];     
+  };
+
+  roundedAverage = Math.ceil(average / marks.length);
+
+  return roundedAverage;
+};
 
 function checkBirthday(birthday) {
     // код для задачи №3 писать здесь
